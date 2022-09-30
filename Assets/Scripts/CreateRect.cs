86495b87cd04756531e8a10eae04d0f4fdf303da
Oleksii Bezkrevnyi <obezkr@gmail.com>
2022-09-30 18:32:59 +0300

    first commit, all files

---
+++ Assets/Scripts/CreateRect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRect : MonoBehaviour
+{
+    public static int size, randWidth, randHeight, height, width;
+    public static int rectCounter=-1;
+    public static bool createRect;
+    [SerializeField] private GameObject singlePixelBluePrefab, singlePixelRedPrefab, singlePixelGreenPrefab, singlePixelYellowPrefab;
+    [SerializeField] private GameObject gameManager; // это сам обьект GameManager, который на сцене
+    [SerializeField] private GameManager gameManagerScript; // а это его скрипт
+    void Start()
+    {
+        gameManagerScript = gameManager.GetComponent<GameManager>();
+        size = gameManagerScript.size;
+        height = gameManagerScript.height;
+        width = gameManagerScript.width;
+        createRect = gameManagerScript.createRect;
+        singlePixelBluePrefab.transform.localScale = new Vector3(size, size, 1);
+        singlePixelRedPrefab.transform.localScale = new Vector3(size, size, 1);
+        singlePixelGreenPrefab.transform.localScale = new Vector3(size, size, 1);
+        singlePixelYellowPrefab.transform.localScale = new Vector3(size, size, 1);
+    }
+
+    void Update()
+    {
+        if (createRect == true)
+        {
+            rectCounter++;
+            createRect = false;
+            randWidth = size * Random.Range(1, 6);
+            randHeight = size * Random.Range(1, 6);
+            /* тут можно заменить префаб каждого цвета на один глобальный префаб,
+             * который просто потом перекрашивать в цвет nowToMove 
+             */
+            if (gameManagerScript.nowToMove == "blue") 
+            {
+                GameObject newRandRect = Instantiate(singlePixelBluePrefab);
+                newRandRect.transform.localScale = new Vector3(randWidth, randHeight, 1);
+                newRandRect.transform.name = rectCounter + "Rect";
+            }
+            else if (gameManagerScript.nowToMove == "red")
+            {
+                GameObject newRandRect = Instantiate(singlePixelRedPrefab);
+                newRandRect.transform.localScale = new Vector3(randWidth, randHeight, 1);
+                newRandRect.transform.name = rectCounter + "Rect";
+            }
+            else if (gameManagerScript.nowToMove == "green")
+            {
+                GameObject newRandRect = Instantiate(singlePixelGreenPrefab);
+                newRandRect.transform.localScale = new Vector3(randWidth, randHeight, 1);
+                newRandRect.transform.name = rectCounter + "Rect";
+            }
+            else if (gameManagerScript.nowToMove == "yellow")
+            {
+                GameObject newRandRect = Instantiate(singlePixelYellowPrefab);
+                newRandRect.transform.localScale = new Vector3(randWidth, randHeight, 1);
+                newRandRect.transform.name = rectCounter + "Rect";  
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PassMove.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassMove : MonoBehaviour
+{
+    [SerializeField] private GameObject gameManager;
+    [SerializeField] private GameManager gameManagerScript;
+    public void Pass()
+    {
+        gameManagerScript = gameManager.GetComponent<GameManager>();
+        string[] colorArray = gameManagerScript.colorArray;
+        string nowToMove = gameManagerScript.nowToMove;
+        int numberOfPlayers = gameManagerScript.numberOfPlayers;
+        int i, numberOfPlayersInGame = 0;
+        // ищем, сколько игроков ещё в игре
+        for (i = 0; i < numberOfPlayers; i++)
+        {
+            if (gameManagerScript.playersScores[i] >= 0)
+            {
+                numberOfPlayersInGame++;
+            }
+        }
+        if (numberOfPlayersInGame > 1) { // если играющих больше чем 1
+            // передаем ход следующему в очереди, у кого очки не отрицательные
+            do
+            {
+                gameManagerScript.nowToMove = colorArray[((Array.IndexOf(colorArray, gameManagerScript.nowToMove) + 1) % numberOfPlayers)];
+                CreateRect.createRect = true;
+            }
+            while (gameManagerScript.playersScores[((Array.IndexOf(colorArray, gameManagerScript.nowToMove)) % numberOfPlayers)] < 0);
+        }
+        else
+        {
+            gameManagerScript.FinishGame();
+        }
+    }
+    public void DestroyRect()
+    {
+        gameManagerScript = gameManager.GetComponent<GameManager>();
+        int penalty = gameManagerScript.penalty;
+        string[] colorArray = gameManagerScript.colorArray;
+        string nowToMove = gameManagerScript.nowToMove;
+        // отнимаем очки
+        gameManagerScript.playersScores[Array.IndexOf(colorArray, nowToMove)] =
+            gameManagerScript.playersScores[Array.IndexOf(colorArray, nowToMove)] - penalty;
+        // уничтожаем прямоугольник и его обводку
+        GameObject objectToDestroy = GameObject.Find(CreateRect.rectCounter + "Rect");
+        Destroy(objectToDestroy);
+        GameObject lRend1 = GameObject.Find("1Line" + CreateRect.rectCounter + "Rect");
+        GameObject lRend2 = GameObject.Find("2Line" + CreateRect.rectCounter + "Rect");
+        GameObject lRend3 = GameObject.Find("3Line" + CreateRect.rectCounter + "Rect");
+        GameObject lRend4 = GameObject.Find("4Line" + CreateRect.rectCounter + "Rect");
+        Destroy(lRend1);
+        Destroy(lRend2);
+        Destroy(lRend3);
+        Destroy(lRend4);
+
+    }
+}
