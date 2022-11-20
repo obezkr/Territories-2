5c9090504929cd3bfb8b80817a243a7c820e2b86
m1r0n0 <95548636+m1r0n0@users.noreply.github.com>
2022-11-20 16:17:04 +0200

    Assignment for 16/11/22 (#1)
    It works by previous settigns and I have no Idea how and why

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
+using System.IO;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int fps = 30, size = 20, penalty = 10, numberOfPlayers = 4, height = 600, width = 600;
+    public int fps = 30, numberOfPlayers = 4, size, penalty, height, width;
     public string nowToMove = "blue", winnerMessage = "";
     public bool createRect = true;
     public string[,] allSquares, allPossibleSquares;
     public string[] colorArray = { "blue", "red", "green", "yellow" };
     public int[] playersScores;
-    private int n, m, i, j, max=0;
+    private int n, m, i, j, max = 0;
+
+    static string path = "settings.ini";
+    static char[] separators = new char[] { ' ', '=', ';' };
+
+    public int Size 
+    {
+        get { return size; } 
+        set { size = value; }
+    }
 
     public void FinishGame() // ось це мені не подобалось, воно має бути не в GameManager, а окремо, но ладно (чи ні?)
     {
         max = -penalty;
-        for (i=0; i<numberOfPlayers; i++)
+        for (i = 0; i < numberOfPlayers; i++)
         {
             if (playersScores[i] > max)
             {
@@ -27,8 +39,45 @@ public class GameManager : MonoBehaviour
         //Debug.Log("finish-game-function");
     }
 
+    async Task ApplySettingsFromFile()
+    {       
+        // асинхронное чтение
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                string[] splittedLine = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                /*foreach(string s in splittedLine)
+                {
+                    Console.WriteLine(s);
+                }*/
+
+                if (splittedLine[0] == "size")
+                    size = Convert.ToInt32(splittedLine[1]);               
+                if (splittedLine[0] == "penalty")
+                    penalty = Convert.ToInt32(splittedLine[1]);
+                if (splittedLine[0] == "height")
+                    height = Convert.ToInt32(splittedLine[1]);
+                if (splittedLine[0] == "width")
+                    width = Convert.ToInt32(splittedLine[1]);
+            }
+        }
+        Debug.Log(size);
+        Debug.Log(penalty);
+        Debug.Log(height);
+        Debug.Log(width);
+    }
+
     void Awake()
     {
+        //(1)Applying settings from file by Miron
+
+        ApplySettingsFromFile();
+
+        //(1)
+
+
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = fps;
         //создаем массив всех клеток поля - кому какие клекти принадлежат
